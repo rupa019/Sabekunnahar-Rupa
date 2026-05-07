f69971aa27bcdba428a40cf6b4d196566c4ccf0d
rupa019 <2023200000019@seu.edu.bd>
2026-05-07 22:15:02 +0600

    Refactor database connection constants

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');        // XAMPP default username
-define('DB_PASS', '');            // XAMPP default password (blank)
+define('DB_USER', 'root');
+define('DB_PASS', '');
 define('DB_NAME', 'meowoof_db');
 
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
