48b83c03c2bb1bf73c2614482ff10e5344263219
rupa019 <2023200000019@seu.edu.bd>
2026-05-07 22:18:34 +0600

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,6 +1,3 @@
--- MeoWoof Database Setup
--- XAMPP এ phpMyAdmin এ গিয়ে এই SQL রান করুন
-
 CREATE DATABASE IF NOT EXISTS meowoof_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 USE meowoof_db;
